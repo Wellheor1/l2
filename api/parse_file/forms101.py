86e4a2325fd7af84008614dd6a21376755d1d046
well <Well.heor@yandex.ru>
2024-11-05 19:23:39 +0800

    .

--- api/parse_file/forms101.py
+++ api/parse_file/forms101.py
@@ -174,7 +174,7 @@ def form_01(request_data):
     file = request_data.get("file")
     other_need_data = request_data.get("other_need_data")
     user = request_data.get("user")
-    company_inn = other_need_data.get("companyInn", None)
+    company_inn = other_need_data.get("companyInn")
     wb = load_workbook(filename=file)
     ws = wb[wb.sheetnames[0]]
     starts = False
