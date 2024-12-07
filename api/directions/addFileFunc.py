520c711b5b697e99215ef414327cb93d4cf9d5f2
well <Well.heor@yandex.ru>
2024-12-07 17:12:55 +0800

    сохранение файла

--- api/directions/addFileFunc.py
+++ api/directions/addFileFunc.py
@@ -1,6 +1,17 @@
+import os.path
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from laboratory.settings import BASE_DIR
 
 
 def add_schema_pdf(request_data):
-
-    print('fdfd')
+    file_in_memory: InMemoryUploadedFile = request_data.get("file")
+    file_content = file_in_memory.file.read()
+    file_name = file_in_memory.name
+    entity_id = request_data.get("entity_id")
+    user = request_data.get("user")
+    path = os.path.join(BASE_DIR, 'media', 'schemas-pdf')
+    with open(f"{path}/service_id_{entity_id}__{file_name}", "wb") as file_on_disk:
+        file_on_disk.write(file_content)
     return True
