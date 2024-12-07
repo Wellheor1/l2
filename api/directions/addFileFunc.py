b73050d9a4415deb79b9f9804db904507aacede0
well <Well.heor@yandex.ru>
2024-12-07 17:13:09 +0800

    fix

--- api/directions/addFileFunc.py
+++ api/directions/addFileFunc.py
@@ -10,7 +10,6 @@ def add_schema_pdf(request_data):
     file_content = file_in_memory.file.read()
     file_name = file_in_memory.name
     entity_id = request_data.get("entity_id")
-    user = request_data.get("user")
     path = os.path.join(BASE_DIR, 'media', 'schemas-pdf')
     with open(f"{path}/service_id_{entity_id}__{file_name}", "wb") as file_on_disk:
         file_on_disk.write(file_content)
