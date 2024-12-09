798384219a6529e868457fe06928833e03cf74e6
well <Well.heor@yandex.ru>
2024-12-09 08:31:30 +0800

    добавление файла

--- api/directions/addFileFunc.py
+++ api/directions/addFileFunc.py
@@ -1,21 +1,27 @@
 import os.path
 from datetime import datetime
 from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from directory.models import Researches
 from laboratory.settings import BASE_DIR, MEDIA_URL
 
 
 def add_schema_pdf(request_data):
     file_in_memory: InMemoryUploadedFile = request_data.get("file")
-    file_content = file_in_memory.file.read()
-    file_name = file_in_memory.name
     entity_id = request_data.get("entity_id")
-    path = os.path.join(BASE_DIR, 'media', 'schemas-pdf', str(entity_id))
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if len(os.listdir(path)) > 1:
-        return False
-    with open(f"{path}/{file_name}", "wb") as file_on_disk:
-        file_on_disk.write(file_content)
+    current_service: Researches = Researches.objects.filter(pk=entity_id).first()
+    if current_service:
+        current_service.schema_pdf = file_in_memory
+        current_service.save()
+    # file_content = file_in_memory.file.read()
+    # file_name = file_in_memory.name
+    # path = os.path.join(BASE_DIR, 'media', 'schemas-pdf', str(entity_id))
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
+    # if len(os.listdir(path)) > 1:
+    #     return False
+    # with open(f"{path}/{file_name}", "wb") as file_on_disk:
+    #     file_on_disk.write(file_content)
     return True
 
 
